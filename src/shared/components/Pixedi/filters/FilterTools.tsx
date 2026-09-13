70bfd5be878b971ef82da0a35e11c60b34de1184
Raycho Ivanov <rastiv.bg@gmail.com>
2026-09-14 00:14:57 +0300

    refactor FilterTools to extract filter logic into useFilters hook

--- src/shared/components/Pixedi/filters/FilterTools.tsx
+++ src/shared/components/Pixedi/filters/FilterTools.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from "react";
-import { filtersData, filterUrlsData } from "../constants";
-import { ActionName, type FilterData } from "../types";
+import { filterUrlsData } from "../constants";
 import { Compare, Filters, PredefinedFilters } from "../assets/icons";
-import { usePixediContext } from "../provider/usePixediContext";
 import {
   Button,
   SaveCloseGroup,
@@ -12,104 +9,28 @@ import {
   Slider,
 } from "../ui";
 import type { SelectOption } from "../ui/select/Select";
-import { emitFilterUpdate } from "../eventBus";
+import { useFilters } from "./useFilters";
 import styles from "./FilterTools.module.css";
 
 export const FilterTools = () => {
   const {
     showCompare,
     previewUrl,
-    getLastFilter,
-    getLastHistoryItem,
-    setCurrentAction,
-    setSidebar,
     toggleCompare,
-    addToHistory,
-    eventBus,
-  } = usePixediContext();
-  const { width, height } = getLastHistoryItem();
-  const { action } = getLastFilter() || {};
-  const args = (action?.args || {}) as Record<string, string | number>;
-  const prevUrl = args?.url as string;
-  const prevFilters = filtersData.map((filter) => ({
-    ...filter,
-    sliderValue: (args[filter.value] ?? filter.sliderValue) as number,
-    rightLabel: `${args[filter.value] ?? filter.sliderValue}${filter.unit}`,
-  }));
-  const prevSaturation = prevFilters.find(
-    (filter) => filter.value === "saturate",
-  )?.sliderValue;
-
-  const [selectedFilter, setSelectedFilter] = useState<string>("saturate");
-  const [selectedUrl, setSelectedUrl] = useState<string>(prevUrl ?? "vintage");
-  const [filters, setFilters] = useState<FilterData[]>(prevFilters);
-  const [sliderValue, setSliderValue] = useState<number>(prevSaturation ?? 0);
-  const [isUrl, setIsUrl] = useState<boolean>(Boolean(prevUrl));
-
-  const filtersObject = Object.fromEntries(
-    filters.map((filter) => [filter.value, filter.sliderValue]),
-  );
-
-  const handleSliderInput = (value: number) => {
-    emitFilterUpdate(eventBus, {
-      ...filtersObject,
-      [selectedFilter]: value,
-    });
-  };
-
-  const selectedFilterItem = filters.find(
-    (filter) => filter.value === selectedFilter,
-  );
-
-  const handleChange = (value: string) => {
-    setSelectedFilter(value);
-    setSliderValue(
-      filters.find((filter) => filter.value === value)?.sliderValue || 0,
-    );
-  };
-
-  const handleChangeWhenUrl = (value: string) => {
-    setSelectedUrl(value);
-    emitFilterUpdate(eventBus, { url: value });
-  };
-
-  const handleSliderChange = (value: number) => {
-    setSliderValue(value);
-    setFilters((prev) =>
-      prev.map((filter) =>
-        filter.value === selectedFilter
-          ? {
-              ...filter,
-              sliderValue: value,
-              rightLabel: `${value}${filter.unit}`,
-            }
-          : filter,
-      ),
-    );
-  };
-
-  const handleSave = () => {
-    addToHistory({
-      width,
-      height,
-      action: {
-        name: ActionName.FILTERS,
-        args: {
-          ...(isUrl
-            ? { url: selectedUrl }
-            : Object.fromEntries(
-                filters.map((filter) => [filter.value, filter.sliderValue]),
-              )),
-        },
-      },
-    });
-    setSidebar(true);
-  };
-
-  const handleClose = () => {
-    setCurrentAction(null);
-    setSidebar(true);
-  };
+    isUrl,
+    toggleIsUrl,
+    filters,
+    selectedFilter,
+    selectedFilterItem,
+    selectedUrl,
+    sliderValue,
+    handleSliderInput,
+    handleSliderChange,
+    handleChange,
+    handleChangeWhenUrl,
+    handleSave,
+    handleClose,
+  } = useFilters();
 
   const getFilterOptionWhenUrl = (option: SelectOption) => {
     return (
@@ -122,24 +43,6 @@ export const FilterTools = () => {
       </div>
     );
   };
-
-  useEffect(() => {
-    if (isUrl) {
-      emitFilterUpdate(eventBus, { url: selectedUrl });
-    } else {
-      emitFilterUpdate(eventBus, {
-        ...filtersObject,
-        [selectedFilter]: sliderValue,
-      });
-    }
-  }, [
-    isUrl,
-    selectedUrl,
-    sliderValue,
-    filtersObject,
-    selectedFilter,
-    eventBus,
-  ]);
 
   return (
     <SurfaceTool className={styles.tools}>
@@ -183,7 +86,7 @@ export const FilterTools = () => {
             variant="outline"
             aria-label={isUrl ? "Filters" : "Predefined Filters"}
             data-tooltip={isUrl ? "Filters" : "Predefined Filters"}
-            onClick={() => setIsUrl(!isUrl)}
+            onClick={toggleIsUrl}
           >
             {isUrl ? <Filters /> : <PredefinedFilters />}
           </Button>
---
+++ src/shared/components/Pixedi/filters/useFilters.tsx
@@ -0,0 +1,141 @@
+import { useEffect, useState } from "react";
+import { filtersData } from "../constants";
+import { ActionName, type FilterData } from "../types";
+import { usePixediContext } from "../provider/usePixediContext";
+import { emitFilterUpdate } from "../eventBus";
+
+export const useFilters = () => {
+  const {
+    showCompare,
+    previewUrl,
+    getLastFilter,
+    getLastHistoryItem,
+    setCurrentAction,
+    setSidebar,
+    toggleCompare,
+    addToHistory,
+    eventBus,
+  } = usePixediContext();
+  const { width, height } = getLastHistoryItem();
+  const { action } = getLastFilter() || {};
+  const args = (action?.args || {}) as Record<string, string | number>;
+  const prevUrl = args?.url as string;
+  const prevFilters = filtersData.map((filter) => ({
+    ...filter,
+    sliderValue: (args[filter.value] ?? filter.sliderValue) as number,
+    rightLabel: `${args[filter.value] ?? filter.sliderValue}${filter.unit}`,
+  }));
+  const prevSaturation = prevFilters.find(
+    (filter) => filter.value === "saturate",
+  )?.sliderValue;
+
+  const [selectedFilter, setSelectedFilter] = useState<string>("saturate");
+  const [selectedUrl, setSelectedUrl] = useState<string>(prevUrl ?? "vintage");
+  const [filters, setFilters] = useState<FilterData[]>(prevFilters);
+  const [sliderValue, setSliderValue] = useState<number>(prevSaturation ?? 0);
+  const [isUrl, setIsUrl] = useState<boolean>(Boolean(prevUrl));
+
+  const filtersObject = Object.fromEntries(
+    filters.map((filter) => [filter.value, filter.sliderValue]),
+  );
+
+  const handleSliderInput = (value: number) => {
+    emitFilterUpdate(eventBus, {
+      ...filtersObject,
+      [selectedFilter]: value,
+    });
+  };
+
+  const selectedFilterItem = filters.find(
+    (filter) => filter.value === selectedFilter,
+  );
+
+  const handleChange = (value: string) => {
+    setSelectedFilter(value);
+    setSliderValue(
+      filters.find((filter) => filter.value === value)?.sliderValue || 0,
+    );
+  };
+
+  const handleChangeWhenUrl = (value: string) => {
+    setSelectedUrl(value);
+    emitFilterUpdate(eventBus, { url: value });
+  };
+
+  const handleSliderChange = (value: number) => {
+    setSliderValue(value);
+    setFilters((prev) =>
+      prev.map((filter) =>
+        filter.value === selectedFilter
+          ? {
+              ...filter,
+              sliderValue: value,
+              rightLabel: `${value}${filter.unit}`,
+            }
+          : filter,
+      ),
+    );
+  };
+
+  const handleSave = () => {
+    addToHistory({
+      width,
+      height,
+      action: {
+        name: ActionName.FILTERS,
+        args: {
+          ...(isUrl
+            ? { url: selectedUrl }
+            : Object.fromEntries(
+                filters.map((filter) => [filter.value, filter.sliderValue]),
+              )),
+        },
+      },
+    });
+    setSidebar(true);
+  };
+
+  const handleClose = () => {
+    setCurrentAction(null);
+    setSidebar(true);
+  };
+
+  const toggleIsUrl = () => setIsUrl(!isUrl);
+
+  useEffect(() => {
+    if (isUrl) {
+      emitFilterUpdate(eventBus, { url: selectedUrl });
+    } else {
+      emitFilterUpdate(eventBus, {
+        ...filtersObject,
+        [selectedFilter]: sliderValue,
+      });
+    }
+  }, [
+    isUrl,
+    selectedUrl,
+    sliderValue,
+    filtersObject,
+    selectedFilter,
+    eventBus,
+  ]);
+
+  return {
+    showCompare,
+    previewUrl,
+    toggleCompare,
+    isUrl,
+    toggleIsUrl,
+    filters,
+    selectedFilter,
+    selectedFilterItem,
+    selectedUrl,
+    sliderValue,
+    handleSliderInput,
+    handleSliderChange,
+    handleChange,
+    handleChangeWhenUrl,
+    handleSave,
+    handleClose,
+  };
+};
